cf3e1a2530e8283624d8b397738766738470cc72
AkshatSharma-hw <34002611+AkshatSharma-hw@users.noreply.github.com>
2019-09-30 14:57:13 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,13 @@
+class Book{
+	private int bookId;
+	private String name;
+	
+	public int getbookId(){
+		return this bookId;
+	}
+	public String getname(){
+		return this name;
+	}
+setter getter
+	System.out.println
+}
